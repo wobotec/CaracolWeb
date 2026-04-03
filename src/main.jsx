7339fc0b94a17b5b031bffbadd49b1c817f4504f
Pedro Panzo <pedropanzo53@gmail.com>
2026-04-03 20:40:10 -0300

    Rem 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-//import 'bootstrap/dist/js/bootstrap.min.js'
-//import 'bootstrap/dist/css/bootstrap.min.css'
-
 import './assets/js/jquery-3.1.1.min.js'
-import './assets/js/bootstrap.min.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
