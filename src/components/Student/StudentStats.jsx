524447fca8339ecb9d49d0b6b6bf4ba81e457911
Pedro Panzo <pedropanzo53@gmail.com>
2026-04-04 21:03:00 -0300

    Ajustado form alunos

--- src/components/Student/StudentStats.jsx
+++ src/components/Student/StudentStats.jsx
@@ -4,25 +4,55 @@ export default function StudentStats({ student }){
     const desactivos = student.filter(s => s.estado !== "Activo").length
 
     return(
-        <div className="row text-center mb-3">
-            <div className="col-md-4">
-                <div className="card p-2">
-                    <h6>TOTAL DE ALUNOS</h6>
-                    <strong className="text-primary">{total}</strong>
-                </div>
-            </div>
-            <div className="col-md-4">
-                <div className="card p-2 ">
-                    <h6>ACTIVOS</h6>
-                    <strong className="text-success">{activos}</strong>
-                </div>
-            </div>
-            <div className="col-md-4">
-                <div className="card p-2 ">
-                    <h6>NÃO ACTIVOS</h6>
-                    <strong className="text-danger">{desactivos}</strong>
+        <div class="row">
+        <div class="col-lg-3">
+            <div class="widget style1 white-bg">
+                <div class="row">
+                    <div class="col-xs-4 text-center line">
+                        <i class="fa fa-user-circle fa-5x"></i>
+                    </div>
+                    <div class="col-xs-8 text-right">
+                        <span> TOTAL DE ALUNOS</span>
+                        <h2 class="font-bold line">4.232</h2>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="col-lg-3">
+            <div class="widget style1 white-bg">
+                <div class="row">
+                    <div class="col-xs-4 totacol">
+                        <i class="fa fa-user-circle fa-5x"></i>
+                    </div>
+                    <div class="col-xs-8 text-right">
+                        <span> ACTIVOS </span>
+                        <h2 class="font-bold totacol">26.34</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="widget style1 white-bg">
+                <div class="row">
+                    <div class="col-xs-4 text-danger">
+                        <i class="fa fa-user-circle fa-5x"></i>
+                    </div>
+                    <div class="col-xs-8 text-right ">
+                        <span> NÃO ACTIVOS </span>
+                        <h2 class="font-bold text-danger">260</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="widget style1">
+                <div class="row">                    
+                    <div class="col-xs-8 text-right ">
+                        <button class="btn btn-primary mt-3" onclick="return adicionar();">Adicionar Aluno</button>                                                
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>   
     )
 }
--- src/components/Student/StudentTable.jsx
+++ src/components/Student/StudentTable.jsx
@@ -1,74 +1,71 @@
-export default function StudentTable({ student }){
+export default function StudentTable({ student }) {
     return (
-        <div className="table-responsive">
-            <table className="table table-hover">
-                <thead className="table-light">
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Sexo</th>
-                        <th>BI</th>
-                        <th>Pai</th>
-                        <th>Mãe</th>
-                        <th>Provincia</th>
-                        <th>Municipio</th>
-                        <th>Residencia</th>
-                        <th>Contacto</th>
-                        <th>Nascimento</th>
-                        <th>Encarregado</th>
-                        <th>Estado</th>
-                        <th>Acções</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {student.map( s => (
-                        <tr
-                            key={s.id}
-                            className={
-                                s.estado === "Desactivo" 
-                                ? "table-danger"
-                                : ""
-                            }
-                        >
-                            <td>{s.id}</td>
-                            <td>{s.name}</td>
-                            <td>{s.sexo}</td>
-                            <td>{s.bi}</td>
-                            <td>{s.father}</td>
-                            <td>{s.mother}</td>
-                            <td>{s.provincia}</td>
-                            <td>{s.municipio}</td>
-                            <td>{s.residencia}</td>
-                            <td>{s.contacto}</td>
-                            <td>{s.nascimento}</td>
-                            <td>{s.encarregado}</td>
-                            <td>
-                                <span
-                                    className={
-                                        s.estado === "Activo"
-                                        ? "text-success"
-                                        : "text-danger"
-                                    }
-                                >
-                                {s.estado}
-                                </span>
-                            </td>
-                            <td>
-                                <button
-                                    className="btn btn-sm btn-light  me-2"
-                                >
-                                    Visualizar
-                                </button>
-                                <button
-                                    className="btn btn-sm btn-light"
-                                >
-                                    Editar
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-content">
+                        <div className="table-responsive">
+                            <table className="table table-hover dataTables-example">
+                                <thead className="table-light">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>NOME</th>
+                                        <th>SEXO</th>
+                                        <th>BI</th>
+                                        <th>RESIDÊNCIA</th>
+                                        <th>CONTACTO</th>
+                                        <th>NASCIMENTO</th>
+                                        <th>ENCARREGADO</th>
+                                        <th>ESTADO</th>
+                                        <th>ACÇÕES</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {student.map(s => (
+                                        <tr
+                                            key={s.id}
+                                            className={
+                                                s.estado === "Desactivo"
+                                                    ? "table-danger"
+                                                    : ""
+                                            }
+                                        >
+                                            <td>{s.id}</td>
+                                            <td>{s.name}</td>
+                                            <td>{s.sexo}</td>
+                                            <td>{s.bi}</td>
+                                            <td>{s.residencia}</td>
+                                            <td>{s.contacto}</td>
+                                            <td>{s.nascimento}</td>
+                                            <td>{s.encarregado}</td>
+                                            <td>
+                                                <span
+                                                    className={
+                                                        s.estado === "Activo"
+                                                            ? "text-success"
+                                                            : "text-danger"
+                                                    }
+                                                >
+                                                    {s.estado}
+                                                </span>
+                                            </td>
+                                            <td>
+                                                <button class="btn btn-success btn-xs" data-bs-toggle="tooltip" data-bs-original-title="Detalhes" id={s.id} onclick="return visualizar('@aluno.Id');">
+                                                    <i class="fa fa-eye"></i>
+                                                </button>
+                                                <button class="btn btn-primary btn-xs" data-bs-toggle="tooltip" data-bs-original-title="Atualizar" id={s.id} onclick="return atualizar('@aluno.Id');">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>        
+    )    
 }
+
